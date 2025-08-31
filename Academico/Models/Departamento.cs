7f15caa3e156c141cf761fb68733e7f64941a623
DevNomura <gu.funke@gmail.com>
2025-08-31 13:25:02 -0300

    crud departamento inserido

---
+++ Academico/Models/Departamento.cs
@@ -0,0 +1,10 @@
+﻿namespace Academico.Models
+{
+    public class Departamento
+    {
+        public int DepartamentoID { get; set; }
+        public string Nome { get; set; }
+        public string Localizacao { get; set; }
+        public List<Aluno> Alunos { get; set; } = new List<Aluno>();    
+    }
+}
